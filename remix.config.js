71b71b6a9b3aef59a069ffe96ffd0e214b28912c
MoroHicham <moro.hicham.dev@gmail.com>
2023-07-14 04:31:20 +0200

    Disable v2 meta data

--- remix.config.js
+++ remix.config.js
@@ -17,10 +17,9 @@ module.exports = {
   serverPlatform: 'neutral',
   serverMinify: process.env.NODE_ENV === 'production',
   future: {
-    v2_meta: true,
-    v2_headers: true,
-    v2_errorBoundary: true,
-    v2_routeConvention: true,
-    v2_normalizeFormMethod: true,
+    v2_meta: false,
+    v2_errorBoundary: false,
+    v2_routeConvention: false,
+    v2_normalizeFormMethod: false,
   },
 };
