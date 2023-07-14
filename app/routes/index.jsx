9e90d92bb1f56fff16fb15ba9facbe5a54350e9c
MoroHicham <moro.hicham.dev@gmail.com>
2023-07-14 04:36:02 +0200

    Ignore product section component

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,14 +1,10 @@
 
 
 // Entry Point Index for Main Layout
-
-
-import ProductSection  from "../components/content/productSection";
-
 export default function Index() {
     return (
         <section className="main-index">
-            <ProductSection />
+           
         </section>
     );
 }
