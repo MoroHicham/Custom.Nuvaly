8117c87965311894fa8ae95d455192fc4356a6a5
MoroHicham <moro.hicham.dev@gmail.com>
2023-07-16 04:28:26 +0200

    Set the option section after main header

--- app/components/header/icons.section.jsx
+++ app/components/header/icons.section.jsx
@@ -21,13 +21,34 @@ export default function IconSection() {
                     </li>
                 </ul>
             </div>
+            <span
+                className="inline-flex items-center justify-center rounded-full bg-purple-100 px-2.5 py-0.5 text-purple-700 mr-1"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="-ms-1 me-1.5 h-4 w-4"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                </svg>
+
+                <p className="whitespace-nowrap text-sm">MAD</p>
+            </span>
             <UserIcon className="h-6 w-6 text-black max-[750px]:hidden" />
             <ShoppingCartIcon className="h-6 w-6 text-black ml-3" />
+          
             <span className="relative flex h-3 w-3">
-                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-pink-500 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
             </span>
-           
+
         </div>
     );
 }
