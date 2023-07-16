d4e956f12cc69cd5d285569459e76af251627cad
MoroHicham <moro.hicham.dev@gmail.com>
2023-07-16 04:28:47 +0200

    Apply new ressources

--- public/scripts.js
+++ public/scripts.js
@@ -46,3 +46,77 @@ $('.video-modal-wrapper').click(function(){
       $('.video-modal-wrapper').css("display", "none");
     }, delayInMilliseconds);
 });
+
+
+// Product list section 
+const slider = document.querySelector('.items');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('activee');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+       
+    velX = 0
+    cancelMomentumTracking();
+
+  });
+  
+  
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+  
+  
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+    beginMomentumTracking();
+  });
+  
+  
+  slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1; //scroll-fast
+    var prevScrollLeft = slider.scrollLeft;
+    slider.scrollLeft = scrollLeft - walk;
+    velX = 1.6*(slider.scrollLeft - prevScrollLeft);
+  });
+  
+  // Momentum 
+  
+  var velX = 0;
+  var momentumID;
+  
+  slider.addEventListener('wheel', (e) => {
+    cancelMomentumTracking();
+  });  
+  
+  function beginMomentumTracking(){
+    cancelMomentumTracking();
+    momentumID = requestAnimationFrame(momentumLoop);
+  }
+  function cancelMomentumTracking(){
+    
+    cancelAnimationFrame(momentumID);
+  }
+  function momentumLoop(){
+    slider.scrollLeft += velX;
+    velX *= 0.97; 
+    if (Math.abs(velX) > 3){
+      momentumID = requestAnimationFrame(momentumLoop);
+    }else{
+      
+    }
+  }
+function snapToPos(pos){
+  slider.scrollLeft = pos
+}
+
+snapToPos(0);
