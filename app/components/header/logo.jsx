03d271f955889870c8a558568d51b67c6f9eabb8
MoroHicham <moro.hicham.dev@gmail.com>
2023-07-23 19:40:30 +0200

    Add link to logo title component

--- app/components/header/logo.jsx
+++ app/components/header/logo.jsx
@@ -1,9 +1,12 @@
+import { Link } from '@remix-run/react';
 export default function Logo() {
     return (
         <div className="flex flex-col text-left">
             <div>
-                 <div className="text-[35px] font-bold  max-[750px]:text-[30px]">NUVALY</div>
-                 <p className="text-[15px] mt-[-11px] text-center max-[750px]:text-[10px]">Regular Skin</p>
+                <Link key={0} to='/'>
+                    <div className="text-[35px] font-bold  max-[750px]:text-[30px]">NUVALY</div>
+                    <p className="text-[15px] mt-[-11px] text-center max-[750px]:text-[10px]">Regular Skin</p>
+                </Link>
             </div>
         </div>
     );
