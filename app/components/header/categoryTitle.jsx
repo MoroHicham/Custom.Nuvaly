5d39aca537bd9c91e94aafe2ed32b3c37ec91729
MoroHicham <moro.hicham.dev@gmail.com>
2023-07-23 03:26:50 +0200

    update category title component style

--- app/components/header/categoryTitle.jsx
+++ app/components/header/categoryTitle.jsx
@@ -1,10 +1,10 @@
 export function CategoryTitle({ categoryName, Character }) {
     return (
         <div className="flex flex-row pt-5 max-[750px]:pt-2 pl-1  max-[750px]:place-content-center">
-            <div className="flex items-center justify-center w-10 h-10 rounded-full border border-[#d0d0d0] max-[750px]:hidden">
-                <p className="text-black font-bold text-lg ">{Character}</p>
+            <div className="flex items-center justify-center w-9 h-9 rounded-full border border-[#d0d0d0] max-[750px]:hidden">
+                <p className="text-black font-bold text-sm">{Character}</p>
             </div>
-            <div className="pt-2 pl-2 font-bold">
+            <div className="pt-2 pl-2 font-bold text-sm">
                 {categoryName}
             </div>
         </div>
