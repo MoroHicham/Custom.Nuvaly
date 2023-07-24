c930c17ef8faf69c1927669a90763f9573b6c980
MoroHicham <moro.hicham.dev@gmail.com>
2023-07-24 05:04:22 +0200

    Sepatare rating component

--- app/components/content/ListCard.jsx
+++ app/components/content/ListCard.jsx
@@ -1,44 +1,32 @@
 import { Link } from '@remix-run/react';
+import  Starts from '../button/starts';
 export default function ListCard({ product, isMobile }) {
 
     const { price, compareAtPrice } = product.variants?.nodes[0] || {};
     const isDiscounted = compareAtPrice?.amount > price?.amount;
 
     return (
-        <div 
+        <div
             key={product.handle}
             className="w-full pb-4 flex flex-row justify-center  overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md hover-zoom">
-            <a className="relative flex-row mx-3 mt-3 flex h-50 overflow-hidden rounded-xl" href="#">
-                <img className="image-container" 
-                src={product.variants.nodes[0].image.url}
-                alt="product image" />
-                <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">39% OFF</span>
-            </a>
-            <div className="mt-4 pl-[1.25rem] pr-[4.25rem] pb-5">
-                <span className="inline-flex items-center px-2 py-1 text-xs font-semibold text-white bg-blue-500 rounded-full">
-                    Boutique Officielle
-                </span>
-                <a href="#">
-                    <h5 className="text-[1rem] tracking-tight text-slate-900">{product.title}</h5>
-                    <div className="flex items-center">
-                        <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">5.0</span>
+            <div>
+                <Link to={`/products/${product.handle}`}>
+                    <a className="relative flex-row mx-3 mt-3 flex h-50 overflow-hidden rounded-xl" href="#">
+                        <img className="image-container"
+                            src={product.variants.nodes[0].image.url}
+                            alt="product image" />
+                        <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">39% OFF</span>
+                    </a>
+                    <div className="mt-4 pl-[1.25rem] pr-[4.25rem] pb-5">
+                        <span className="inline-flex items-center px-2 py-1 text-xs font-semibold text-white bg-blue-500 rounded-full">
+                            Boutique Officielle
+                        </span>
+                        <a href="#">
+                            <h5 className="text-[1rem] tracking-tight text-slate-900">{product.title}</h5>
+                          <Starts />
+                        </a>
                     </div>
-                </a>
+                </Link>
             </div>
             <div className="flex flex-col basis-1/2 items-end justify-between pr-5">
                 <div className="mt-2 mb-5 flex flex-col items-center justify-between">
